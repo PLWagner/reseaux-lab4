2b4d9c95e3a6c8dc993ed80d5623c1414d9d4cb1
PLWagner <pierreluc.wagner@gmail.com>
2015-03-10 19:36:51 -0400

    Serveur, test in coming

--- src/part1/Serveur.java
+++ src/part1/Serveur.java
@@ -3,11 +3,15 @@ import java.net.*;
 import java.io.*;
 
 public class Serveur {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] zero) throws Exception {
 
         ServerSocket serverSocket = null;
+        Socket socketDuServeur ;
         try {
             serverSocket = new ServerSocket(4444);
+            System.out.println("Le serveur est à l'écoute du port "+ serverSocket.getLocalPort());
+            socketDuServeur = serverSocket.accept(); 
+			System.out.println("Un être s'est connecté !");
         } catch (IOException e) {
             System.err.println("Could not listen on port: 4444.");
             System.exit(1);
