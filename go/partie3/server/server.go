a0f900383c386614eda52d53f376ff975b6d27b3
aviau <alexandre.viau@savoirfairelinux.com>
2015-03-12 19:33:57 -0400

    Added redirectionSeulement and DNSFile flags

--- go/partie3/server/server.go
+++ go/partie3/server/server.go
@@ -8,18 +8,33 @@ import (
 	"strings"
 )
 
+var redirectionSeulement bool
+var dnsFile string
+
 func main() {
 	var port = flag.String("port", "", "Port Number")
+	var redirectOnlyFlag = flag.Bool("redirectionSeulement", false, "Should the server only redirect")
+	var dnsFileFlag = flag.String("DNSFile", "", "DNS file path")
 	flag.Parse()
 
+	// Parse and init the arguments
 	if *port == "" {
 		fmt.Println("You must specify --port")
 		os.Exit(1)
 	}
 
+	if *dnsFileFlag == "" {
+		fmt.Println("You must specify --DNSFile")
+		os.Exit(1)
+	}
+
+	redirectionSeulement = *redirectOnlyFlag
+
+	// Create the Socket
 	addr, _ := net.ResolveUDPAddr("udp", ":"+*port)
 	sock, _ := net.ListenUDP("udp", addr)
 
+	// Main UDP loop
 	for {
 		buf := make([]byte, 1024)
 		rlen, _, err := sock.ReadFromUDP(buf)
@@ -50,7 +65,16 @@ func handlePacket(packet []byte) {
 			domainName = append(domainName, string(packet[qnameIndex+1:qnameIndex+1+domainFieldLenght]))
 			qnameIndex += domainFieldLenght + 1
 		}
-		fmt.Println("REQUEST pour: " + strings.Join(domainName, "."))
+		QName := strings.Join(domainName, ".")
+		fmt.Println("REQUEST QNAME: " + QName)
+
+		// *Si le mode est redirection seulement
+		if redirectionSeulement {
+			// *Rediriger le paquet vers le serveur DNS
+		} else {
+			// *Sinon
+			// *Rechercher l'adresse IP associe au Query Domain name dans le fichier de correspondance de ce serveur
+		}
 
 	} else { // ****** Dans le cas d'un paquet reponse *****
 		fmt.Print("ELLO")
