94bda5fc0b8fdf3b62e7f04c49008f4187138251
aviau <alexandre.viau@savoirfairelinux.com>
2015-03-12 12:58:54 -0400

    Applied gofmt

--- go/server.go
+++ go/server.go
@@ -1,49 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "flag"
-    "net"
-    "bufio"
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
 )
-
 
 func main() {
 
-    var port = flag.String("port", "", "Port Number")
-    flag.Parse()
+	var port = flag.String("port", "", "Port Number")
+	flag.Parse()
 
-    if *port == "" {
-        fmt.Println("You must specify --port")
-        os.Exit(1)
-    }
+	if *port == "" {
+		fmt.Println("You must specify --port")
+		os.Exit(1)
+	}
 
-    ln, err := net.Listen("tcp", ":" + *port)
-    if err != nil {
-        println("Listen failed:", err.Error())
-        os.Exit(1)
-    }
+	ln, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		println("Listen failed:", err.Error())
+		os.Exit(1)
+	}
 
-    for {
-        con, err := ln.Accept()
-        if err != nil {
-            println("Could not accept:", err.Error())
-        }
-        go echo(con)
-    }
+	for {
+		con, err := ln.Accept()
+		if err != nil {
+			println("Could not accept:", err.Error())
+		}
+		go echo(con)
+	}
 
 }
 
 func echo(conn net.Conn) {
-    connbuf := bufio.NewReader(conn)
-    for{
-        str, err := connbuf.ReadString('\n')
-        if len(str)>0 {
-            fmt.Print(str)
-        }
-        if err!= nil {
-            break
-        }
-    }
+	connbuf := bufio.NewReader(conn)
+	for {
+		str, err := connbuf.ReadString('\n')
+		if len(str) > 0 {
+			fmt.Print(str)
+		}
+		if err != nil {
+			break
+		}
+	}
 }
